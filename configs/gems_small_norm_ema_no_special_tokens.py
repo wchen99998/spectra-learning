78ff817bc6d35783bf41c06b2df4271f0a918147
wuhao <wuhao@wuhao99.com>
2026-05-11 16:31:32 +0000

    refactor: remove deprecated configuration files for gems_small and related settings

--- configs/gems_small_norm_ema_no_special_tokens.py
+++
@@ -1,9 +0,0 @@
-from configs.gems_small_norm_ema import get_config as get_base_config
-
-
-def get_config():
-    cfg = get_base_config()
-    cfg.encoder_use_cls_token = False
-    cfg.encoder_num_register_tokens = 0
-    cfg.predictor_num_register_tokens = 0
-    return cfg
--- configs/gems_small_norm_ema_no_target_projector.py
+++
@@ -1,7 +0,0 @@
-from configs.gems_small_norm_ema import get_config as get_base_config
-
-
-def get_config():
-    cfg = get_base_config()
-    cfg.target_projector_dim = -1
-    return cfg
